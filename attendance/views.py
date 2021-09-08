d809a3843fa3daec0e83f366a5bd5adc0d9a2d9c
ketanmore2002 <moreketan2002@gmail.com>
2021-09-08 21:40:19 +0530

    k

--- attendance/views.py
+++ attendance/views.py
@@ -8,16 +8,7 @@ from django.contrib.auth.models import User
 
 
 
-from django_tables2 import SingleTableView
 
-from .models import s_attendance
-from .tables import s_attendanceTable
-
-
-class s_attendanceTable(SingleTableView):
-    model = s_attendance
-    table_class = s_attendanceTable
-    template_name = 'tables.html'
 
 
 
--- store/settings.py
+++ store/settings.py
@@ -41,7 +41,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.sites',
-    'django_tables2',
     'ordersapp',
     'attendance',
     'teachers_profile',
