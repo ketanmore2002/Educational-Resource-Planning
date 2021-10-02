00544c6b15829b5d1c1209be704dcef88b2c4835
ketanmore2002 <moreketan2002@gmail.com>
2021-10-02 12:23:09 +0530

    c

--- store/settings.py
+++ store/settings.py
@@ -174,7 +174,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 SITE_ID = 1 
 
-LOGIN_REDIRECT_URL = "/profile"
+LOGIN_REDIRECT_URL = "/"
 
 # Provider specific settings
 SOCIALACCOUNT_PROVIDERS = {
